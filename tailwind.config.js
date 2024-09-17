8026e22ce2279928f544fbd1143dc4d7b2dcc09e
Allen G. Clarke <clarkeallen241@gmail.com>
2024-09-17 00:44:39 +0000

    remove tailwind prettier formatter and reinstall tailwind along with postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+    ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
