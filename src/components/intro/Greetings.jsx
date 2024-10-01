c5a96bcc6c73522ab565efdcf0d56a2b1e23fb60
Allen G. Clarke <clarkeallen241@gmail.com>
2024-10-01 00:07:06 +0000

    Temporary commit
    modify the introductory section. this will later be squashed

--- src/components/intro/Greetings.jsx
+++ src/components/intro/Greetings.jsx
@@ -1,11 +1,11 @@
-import background from "../../assets/bgimage.jpg";
+import background from "../../assets/greetings-bg.jpg";
 
 const Greetings = () => {
   return (
     <div
       className="h-96 bg-no-repeat bg-center bg-cover"
       style={{
-        backgroundImage: `linear-gradient(rgba(0,0,255,0.3),rgba(0,0,255,0.7)),url("${background}")`
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.456),rgba(0,0,0,0.789)),url("${background}")`
       }}
     >
       <h1 className="text-white font-black text-4xl pt-12 px-4">
@@ -14,7 +14,11 @@ const Greetings = () => {
       <p className="font-black font-mono text-white pt-1 px-4">
         Crafting your idea into stunning architectural master plan
       </p>
-      <button className="font-black text-white rounded-2xl bg-blue-700 p-2 ml-4 mt-4 border border-amber-50">
+
+      <button
+        href="#"
+        className="font-black text-white rounded-2xl p-2 ml-4 mt-4 border border-amber-50 backdrop-blur-2xl"
+      >
         Contact Us <i className="bx bx-phone"></i>
       </button>
     </div>
