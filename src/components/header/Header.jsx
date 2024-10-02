6ccaae6d3fcd0d9cd9c04725f6d18f06fed40acc
Allen G. Clarke <clarkeallen241@gmail.com>
2024-10-02 09:40:27 +0000

    header temporary commit
    this will be squashed into the main header creation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,18 @@
-import logo from "../../assets/clarke-drafting-logo-blue.png";
+import logo from "../../assets/clarke-drafting-logo-black.png";
 
 const Header = () => {
   return (
-    <header>
-      <div className="flex flex-row justify-between items-center py-1.5 px-2">
-        <div>
+    <header className="bg-white mb-1">
+      <div className="flex flex-row justify-start items-center py-1.5 px-2">
+        <div className="flex items-center justify-between">
           <img
             src={logo}
             alt="Clarke Drafting logo"
             className="h-10 w-10 rounded-full"
           />
-        </div>
-        <div>
-          <button className="">
-            <i className="bx bx-moon"></i>
-          </button>
+          <p className="text-black text-3xl font-black font-sans ml-1">
+            Clarke Drafting
+          </p>
         </div>
       </div>
     </header>
