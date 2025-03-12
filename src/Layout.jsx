e898a812696d2d90600ec2a0135019410d375d81
Allen G. Clarke <clarkeallen241@gmail.com>
2025-03-12 03:15:41 +0000

    vercel web analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -8,6 +9,7 @@ const Layout = () => {
       <Header />
       <Outlet />
       <Footer />
+      <Analytics />
     </>
   );
 };
